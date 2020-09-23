71a0f287d221859f663421f82423444e17f2665d
Lydia Zhang <art4everyan@gmail.com>
2020-09-23 08:08:26 -0400

    modify pdf to business card size

--- LYDAppDevPortfolioTracker/LYDAppDevPortfolioTracker/PDF/PDF.swift
+++ LYDAppDevPortfolioTracker/LYDAppDevPortfolioTracker/PDF/PDF.swift
@@ -29,20 +29,18 @@ class PDF {
         
         let format = UIGraphicsPDFRendererFormat()
         format.documentInfo = pdfMetaData as [String: Any]
-        let pageWidth = 8.5 * 72.0
-        let pageHeight = 11 * 72.0
+        let pageHeight = 8.5 * 2.0
+        let pageWidth = 11.0 * 2.0
         let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
-        
         let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
         
         let data = renderer.pdfData { (context) in
             context.beginPage()
             
             addImage(image: UIImage(named: "56")!,
-            pageRect: pageRect,
             width: CGFloat(pageWidth), height: CGFloat(pageHeight))
             let titleSize = addTitle(pageRect: pageRect)
-            addBodyText(pageRect: pageRect, textTop: titleSize + 36.0)
+            addBodyText(pageRect: pageRect, textTop: titleSize + 3.0)
             
         }
         
@@ -50,7 +48,7 @@ class PDF {
     }
     
     func addTitle(pageRect: CGRect) -> CGFloat {
-        let titleFont = UIFont.systemFont(ofSize: 50.0, weight: .bold)
+        let titleFont = UIFont.systemFont(ofSize: 2.0, weight: .bold)
         let titleAttributes: [NSAttributedString.Key: Any] =
             [NSAttributedString.Key.font: titleFont]
 
@@ -62,7 +60,7 @@ class PDF {
         let titleStringSize = attributedTitle.size()
         let titleStringRect = CGRect(
             x: (pageRect.width - titleStringSize.width) / 2.0,
-            y: 36,
+            y: 0,
             width: titleStringSize.width,
             height: titleStringSize.height
         )
@@ -73,7 +71,7 @@ class PDF {
     }
     
     func addBodyText(pageRect: CGRect, textTop: CGFloat) {
-        let textFont = UIFont.systemFont(ofSize: 12.0, weight: .regular)
+        let textFont = UIFont.systemFont(ofSize: 1.0, weight: .regular)
 
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .natural
@@ -85,21 +83,21 @@ class PDF {
         ]
 
         let attributedText = NSAttributedString(
-            string: "\(github) \n \(intro)",
+            string: "Github Handle:  \(github) \nIntroduction:   \(intro)",
             attributes: textAttributes
         )
 
         let textRect = CGRect(
-            x: 10,
+            x: 1,
             y: textTop,
-            width: pageRect.width - 20,
-            height: pageRect.height - textTop - pageRect.height / 5.0
+            width: pageRect.width - 10,
+            height: pageRect.height - textTop - pageRect.height / 2.0
         )
 
         attributedText.draw(in: textRect)
     }
     
-    func addImage(image: UIImage, pageRect: CGRect, width: CGFloat, height: CGFloat) {
+    func addImage(image: UIImage, width: CGFloat, height: CGFloat) {
         
         let imageRect = CGRect(x: 0, y: 0,
                                width: width, height: height)
